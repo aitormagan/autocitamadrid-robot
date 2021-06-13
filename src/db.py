4369e418e1768587d60d97f06c4d865ff66199da
Aitor Magán <amagan@conwet.com>
2021-06-13 12:47:39 +0200

    Add checker

--- src/db.py
+++ src/db.py
@@ -3,11 +3,32 @@ import boto3
 TABLE_NAME = 'AutocitaMadridNotifications'
 CLIENT = boto3.client('dynamodb')
 
+__USER_ID = "user_id"
+__NAME = "name"
+__AGE = "age"
+__NOTIFIED = "notified"
 
-def save_notification(user_id, name, age):
-    CLIENT.put_item(TableName=TABLE_NAME, Item={"user_id": {"S": str(user_id)}, "name": {"S": name},
-                                                "age": {"N": str(age)}})
+
+def save_notification(user_id, name, age, notified=False):
+    CLIENT.put_item(TableName=TABLE_NAME, Item={__USER_ID: {"S": str(user_id)}, __NAME: {"S": name},
+                                                __AGE: {"N": str(age)}, __NOTIFIED: {"BOOL": notified}})
 
 
 def delete_notification(user_id):
-    CLIENT.delete_item(TableName=TABLE_NAME, Key={"user_id": {"S": str(user_id)}})
+    CLIENT.delete_item(TableName=TABLE_NAME, Key={__USER_ID: {"S": str(user_id)}})
+
+
+def get_non_notified_people():
+    paginator = CLIENT.get_paginator('scan')
+    dynamo_items = paginator.paginate(TableName=TABLE_NAME).build_full_result().get("Items", [])
+    non_notified = []
+
+    for item in dynamo_items:
+        if not item.get(__NOTIFIED).get("BOOL"):
+            non_notified.append({
+                "name": item[__NAME]["S"],
+                "age": int(item[__AGE]["N"]),
+                "user_id": item[__USER_ID]["S"]
+            })
+
+    return non_notified
---
+++ src/main_checker.py
@@ -0,0 +1,35 @@
+import requests
+from datetime import datetime
+from telegram_helpers import send_text
+from db import save_notification, get_non_notified_people
+
+
+def notify(user_info):
+    message = f"¡Buenas noticias {user_info['name']}! El sistema de vacunación de la Comunidad de Madrid ya permite " \
+              f"pedir cita a gente con {user_info['age']} o menos. !Corre y pide tu cita en " \
+              f"https://autocitavacuna.sanidadmadrid.org/ohcitacovid/!"
+    send_text(user_info["user_id"], message)
+
+
+def mark_as_notified(user_info):
+    save_notification(user_info["user_id"], user_info["name"], user_info["age"], True)
+
+
+def main():
+
+    data = requests.get("https://autocitavacuna.sanidadmadrid.org/ohcitacovid/assets/config/app-config.json",
+                        verify=False).json()
+
+    max_birthday = datetime.strptime(data["dFin_Birthday"], "%d/%m/%Y")
+    curr_date = datetime.now()
+    min_years = curr_date.year - max_birthday.year
+    non_notified = get_non_notified_people()
+
+    for person in non_notified:
+        if min_years <= person.get("age"):
+            notify(person)
+            mark_as_notified(person)
+
+
+if __name__ == '__main__':
+    main()
