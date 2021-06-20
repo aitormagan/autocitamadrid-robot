ab93592b43171f87af2c5a5db57bc8e5564751b7
Aitor Magán <amagan@conwet.com>
2021-06-20 16:53:55 +0200

    Avoid exception on 403 (code)

--- src/telegram_helpers.py
+++ src/telegram_helpers.py
@@ -7,6 +7,6 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN")
 def send_text(chat_id, message):
     send_text = f'https://api.telegram.org/bot{BOT_TOKEN}/sendMessage?chat_id={chat_id}&parse_mode=Markdown&text={message}'
     response = requests.get(send_text, timeout=5)
-    if response.status_code > 400:
+    if response.status_code not in [200, 400, 403]:
         response.raise_for_status()
     return response.json()
