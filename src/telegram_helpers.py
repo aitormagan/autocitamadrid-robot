3a31461acd65f4557acc5eaef6fbdf7cd8a526af
Aitor Magán <amagan@conwet.com>
2021-06-20 13:08:07 +0200

    Raise for status

--- src/telegram_helpers.py
+++ src/telegram_helpers.py
@@ -7,4 +7,5 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN")
 def send_text(chat_id, message):
     send_text = f'https://api.telegram.org/bot{BOT_TOKEN}/sendMessage?chat_id={chat_id}&parse_mode=Markdown&text={message}'
     response = requests.get(send_text, timeout=5)
+    response.raise_for_status()
     return response.json()
