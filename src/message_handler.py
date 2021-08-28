41a909648fe31d45a2f1a452464c50a7012ce9a6
Aitor Magán <amagan@conwet.com>
2021-08-28 11:09:28 +0200

    Recover current_age

--- src/message_handler.py
+++ src/message_handler.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 from aws_lambda_powertools import Logger
 from src import telegram_helpers
 from src import db
@@ -26,6 +27,8 @@ def handle_update(update):
         elif message in ["/cancel"]:
             answer = "Si quieres borrar tu suscripción sólo tienes que detener el bot. Para ello, accede al perfil y " \
                      "haz click en *Detener bot*."
+        elif message == "/currentage":
+            answer = handle_current_age(update)
 
         update["answer"] = answer
         logger.info(update)
@@ -36,3 +39,13 @@ def handle_update(update):
         user_id = update["my_chat_member"]["from"]["id"]
         logger.info(f"User with id {user_id} stopped the bot")
         db.delete_notification(user_id)
+
+
+def handle_current_age(_):
+    min_years = db.get_min_years()
+    max_year_of_birth = datetime.now().year - min_years
+    message = f"El sistema de autocita permite pedir cita a personas nacidas en {max_year_of_birth} o antes. " \
+              f"Si cumples con este criterio, no esperes más y ve a " \
+              f"🔗 https://autocitavacuna.sanidadmadrid.org/ohcitacovid para pedir tu cita."
+
+    return message
