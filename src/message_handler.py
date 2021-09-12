bdd625a6a6300b3b141000c1502a09c26ddbf47b
Aitor Magán <amagan@conwet.com>
2021-09-12 12:54:13 +0200

    Remove # part

--- src/message_handler.py
+++ src/message_handler.py
@@ -16,7 +16,7 @@ def handle_update(update):
 
     if user_id:
         answer = "¡Ahora puedes vacunarte sin cita previa 🎉! Tienes más info ➡️ [aquí]" + \
-                 "(https://www.comunidad.madrid/servicios/salud/vacunacion-frente-coronavirus-comunidad-madrid#plan-vacunacion)" + \
+                 "(https://www.comunidad.madrid/servicios/salud/vacunacion-frente-coronavirus-comunidad-madrid)" + \
                  "\n\n¡No esperes más, vacúnate 💉 ya!"
         if message in ["/start", "/help"]:
             answer = f"¡Hola {name}! Bienvenidx al sistema de notificación de vacunación.\n\n{answer}"
--- src/telegram_helpers.py
+++ src/telegram_helpers.py
@@ -9,7 +9,7 @@ def send_text(chat_id, message):
     if STAGE == 'dev':
         message = f"⚠️ Este es un bot de pruebas y su funcionamiento no está garantizado 😥. Te recomendamos utilizar " \
                   f"la versión productiva que puedes encontrar en 👉 https://t.me/vacunacovidmadridbot \n\n{message}"
-    send_text = f'https://api.telegram.org/bot{BOT_TOKEN}/sendMessage?chat_id={chat_id}&parse_mode=MarkdownV2&text={message}'
+    send_text = f'https://api.telegram.org/bot{BOT_TOKEN}/sendMessage?chat_id={chat_id}&parse_mode=Markdown&text={message}'
     response = requests.get(send_text, timeout=5)
     if response.status_code not in [200, 400, 403]:
         response.raise_for_status()
